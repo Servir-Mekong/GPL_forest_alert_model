ca8b9dbbbfb09a7ff1b8c1c766956154327a4b00
Ate <poortinga.ate@gmail.com>
2021-01-11 14:22:46 +0700

    updated sampling

--- 3_running_sampling.py
+++ 3_running_sampling.py
@@ -1,8 +1,16 @@
 import ee
 from time import sleep
 from utils import task_monitor
+import math
+import numpy as np
+
+from numpy.random import seed
+from numpy.random import rand
 
 ee.Initialize()
+
+seed(10)
+values = rand(50000)
 
 # Load the export monitor
 EXPORT_MONITOR = task_monitor.GEETaskMonitor()
@@ -12,122 +20,115 @@ def main ():
     # print('Sleeping')
     # sleep(60 * 60 * 6)
     
-    # Define the username
-    username = "JohnBKilbride"
     
-    # Define the output location
-    output_dir = "SERVIR/real_time_monitoring"
+    outputBucket = "bucketsvmk"
+    folder = "alerts/gladAlertv64v4"
     
+    featureNames = ['VH_after0', 'VH_before0', 'VH_before1', 'VH_before2', 'VV_after0', 'VV_before0', 'VV_before1', 'VV_before2', 'glad_alert', 'non_alert']
+
     # Define kernel size 
     kernel_size = 64
     image_kernel = get_kernel(kernel_size)
     
-    # Cloud Storage Parameters
-    cloud_bucket = "kilbride_bucket_1"
-    cloud_folder = "glad_alert_records/"
     
     # Get the projection that is needed for the study area
     projection = ee.Projection('EPSG:32648')
     
     # Load in the GLAD Alert Images
-    glad_alerts = ee.Image('users/JohnBKilbride/SERVIR/real_time_monitoring/glad_alerts_2019_to_2020')
     
-    # Load in the sample locations
-    sample_locations = ee.FeatureCollection("users/JohnBKilbride/SERVIR/real_time_monitoring/sample_locations_2019_2020_train_val_test_50k")
-        # .randomColumn(None, 43214).sort('random').limit(1)
+    year = 2019
     
-    #################################### Ignore stuff below #######################
+    # Import alert features with Landsat Observation date
     
-    # Load in the topographic metrics
-    topography = load_topographic()
+    sample_locations =ee.FeatureCollection("projects/cemis-camp/assets/alerts/alerts"+str(year))
     
-    # Loop through the points
-    num_samples = sample_locations.size().getInfo()
-    sample_list = sample_locations.toList(num_samples)
-    print('\nInitiating {num_exports} exports.'.format(num_exports=num_samples))
-    for i in range(0, num_samples):
-        
-        print('\nExporting record {i}'.format(i=i))
-        
-        # Get the next feature by index and cast
-        current_location = ee.Feature(sample_list.get(i))
-        
-        # Get the geometry from the current feature
-        location_geo = current_location.geometry()
-        
-        # Get the dataset type
-        dataset_subset = ee.Number(current_location.get('dataset_subset')).toInt8().getInfo()
-        if dataset_subset == 1:
-            dataset_name = 'train'
-        elif dataset_subset == 2:
-            dataset_name = 'validation'
-        elif dataset_subset == 3:
-            dataset_name = 'test'
-        
-        # Get the day and year of each of the dates
-        before_day = ee.Number(current_location.get('start_day'))  
-        before_year = ee.Number(current_location.get('start_year'))
-        after_day = ee.Number(current_location.get('alert_day'))
-        after_year = ee.Number(current_location.get('alert_year'))
-        
-        # Get the Before and After Images
-        sentinel = generate_before_after_image(before_day, after_day, before_year, after_year, location_geo)
-        
-        # Get the Labels
-        glad_label = calculate_glad_label(glad_alerts, before_day, after_day, before_year, after_year)
-        
-        # Combine the labels
-        all_bands = ee.Image.cat(sentinel, topography, glad_label).toFloat()
-        
-        # # Sample with point and neighborhoodToArray()
-        # arrays = all_bands.neighborhoodToArray(image_kernel)
-        # output = arrays.sampleRegions(
-        #     collection = ee.FeatureCollection([current_location]),
-        #     properties = ["VV_before","VH_before","VV_after","VH_after","glad_alert"],
-        #     scale = 10, 
-        #     projection = projection, 
-        #     geometries = False
-        #     )
-            
-        # # Export the example to Google Cloud Storage
-        # task_1 = ee.batch.Export.table.toCloudStorage(
-        #     collection = output, 
-        #     description = 'Export-GLAD-' + str(i), 
-        #     bucket = cloud_bucket, 
-        #     fileNamePrefix = cloud_folder + '/glad_alert_' + str(i), 
-        #     fileFormat = 'TFRecord', 
-        #     selectors = ["VV_before","VH_before","VV_after","VH_after","glad_alert"]
-        #     )
-        # task_1.start()
-        
-        # For debugging
-        # if i < 10:
-        export_geometry = location_geo.buffer(ee.Number(kernel_size/2).multiply(10), ee.ErrorMargin(0.0001, "projected"), projection) \
-                .bounds(ee.ErrorMargin(0.0001, "projected"), projection)
-        task_2 = ee.batch.Export.image.toDrive(
-            image = all_bands, 
-            description = 'GLAD-Image-Export-' + str(i),
-            folder = 'alert_' + str(kernel_size) + '_' + str(kernel_size) + '_before_after_delta_topo',
-            fileNamePrefix = 'glad_'+ dataset_name + '_' + str(i), 
-            region = export_geometry, 
-            scale = 10,
-            crs = projection,
-            maxPixels = 1e6
-            )
-        task_2.start()
-        
-        # Check for completed exports every 250 iterations
-        if  i % 250 == 0:
-            check_for_monitor_capacity()
-            
-        EXPORT_MONITOR.add_task('export_' + str(i), task_2)
-            
-    # Run the monitoring of the exports
-    EXPORT_MONITOR.monitor_tasks()
-    EXPORT_MONITOR.reset_monitor()
-    print('...export completed.')    
-            
-    return None
+    labels = ee.Image("projects/cemis-camp/assets/glad2020/alert" + str(year)[2:]+ "_100E_10N_110E_20N")
+    
+    #label = label.unmask(0)	
+    label = labels.gt(0).rename(['glad_alert'])
+    nonLabel = label.remap([0,1],[1,0]).rename(["non_alert"])
+
+    # Import Sentinel-1 Collection 
+    s1 =  ee.ImageCollection('COPERNICUS/S1_GRD')\
+			.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))\
+			.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))\
+			.filter(ee.Filter.eq('orbitProperties_pass', 'DESCENDING'))\
+			.filter(ee.Filter.eq('instrumentMode', 'IW'))\
+			.filterBounds(sample_locations.geometry().bounds())\
+			.map(terrainCorrection)\
+			.map(applySpeckleFilter)\
+			.map(addRatio)
+
+
+    beforeDate = ee.Date.fromYMD(year,1,1) 
+    afterDate = ee.Date.fromYMD(year,12,31)
+    
+    before = createSeriesBefore(s1,beforeDate)
+    after = createSeriesAfter(s1,afterDate)
+
+    image = before.addBands(after).addBands(label).addBands(nonLabel).unmask(0,False)
+    sample_locations = sample_locations.toList(15000)
+
+    for i in range(0,100,1):
+	p1 = ee.Feature(sample_locations.get(i))
+	
+	bounds = p1.geometry().buffer(320).bounds()
+	points = ee.FeatureCollection.randomPoints(region=bounds, points=10, seed=i)
+	
+	neighborhood = image.neighborhoodToArray(image_kernel) 
+	trainingData = neighborhood.sample(region = points,scale= 10,tileScale= 16, geometries= True)
+	
+	sample = ee.Feature(trainingData.first())
+	
+	if values[i]<=0.1:
+	    trainFilePrefix = "/validation/valid" + str(year) + "_" + str(i).zfill(4)
+	elif values[i]>0.1 and values[i] <= 0.3:
+	    trainFilePrefix = "/testing/test" + str(year) + str(i).zfill(4)
+	else:
+	    trainFilePrefix = "/training/train_" + str(year) + str(i).zfill(4)
+
+	trainingTask = ee.batch.Export.table.toCloudStorage(collection= ee.FeatureCollection(trainingData),
+						    description= "trainpatch"+str(i),
+						    fileNamePrefix= folder+ trainFilePrefix,
+						    bucket= outputBucket,
+						    fileFormat= 'TFRecord',
+						    selectors= featureNames)
+							    
+	trainingTask.start()
+
+
+
+def createSeriesBefore(collection,date,iters=3,nday =12):
+    
+    iterations = range(1,iters*nday,nday)
+    
+    names = ["_before{:01d}".format(x) for x in range(0,iters,1)]
+
+    def returnCollection(day,name):
+	start = ee.Date(date).advance(-day,"days")
+	end = ee.Date(date).advance(-day+nday,"days")
+	bandNames = ["VV"+name,"VH"+name,"ratio"+name]
+	return ee.Image(collection.filterDate(start,end).mean())\
+				  .select(["VV","VH","ratio"],bandNames)\
+				  .set("system:time_start",start)
+    
+    return toBands(ee.ImageCollection.fromImages(map(returnCollection,iterations,names)))
+
+def createSeriesAfter(collection,date,iters=1,nday =12):
+    
+    iterations = range(1,iters*nday,nday)
+    
+    names = ["_after{:01d}".format(x) for x in range(0,iters,1)]
+
+    def returnCollection(day,name):
+	start = ee.Date(date).advance(day,"days")
+	end = ee.Date(date).advance(day+nday,"days")
+	bandNames = ["VV"+name,"VH"+name,"ratio"+name]
+	return ee.Image(collection.filterDate(start,end).mean())\
+				  .select(["VV","VH","ratio"],bandNames)\
+				  .set("system:time_start",start)
+    
+    return toBands(ee.ImageCollection.fromImages(map(returnCollection,iterations,names)))    
 
 # Produces a kernel of a given sized fro sampling in GEE
 def get_kernel (kernel_size):
@@ -236,6 +237,149 @@ def  calculate_glad_label(glad_alerts, before_day, after_day, before_year, after
         }).toByte()
     
     return combined
+
+# Implementation by Andreas Vollrath (ESA), inspired by Johannes Reiche (Wageningen)
+def terrainCorrection(image):
+    date = ee.Date(image.get('system:time_start'))
+    imgGeom = image.geometry()
+    srtm = ee.Image('USGS/SRTMGL1_003').clip(imgGeom)  # 30m srtm 
+    sigma0Pow = ee.Image.constant(10).pow(image.divide(10.0))
+
+    #Article ( numbers relate to chapters) 
+    #2.1.1 Radar geometry 
+    theta_i = image.select('angle')
+    phi_i = ee.Terrain.aspect(theta_i).reduceRegion(ee.Reducer.mean(), theta_i.get('system:footprint'), 1000).get('aspect')
+
+    #2.1.2 Terrain geometry
+    alpha_s = ee.Terrain.slope(srtm).select('slope')
+    phi_s = ee.Terrain.aspect(srtm).select('aspect')
+
+    # 2.1.3 Model geometry
+    # reduce to 3 angle
+    phi_r = ee.Image.constant(phi_i).subtract(phi_s)
+
+    #convert all to radians
+    phi_rRad = phi_r.multiply(math.pi / 180)
+    alpha_sRad = alpha_s.multiply(math.pi / 180)
+    theta_iRad = theta_i.multiply(math.pi / 180)
+    ninetyRad = ee.Image.constant(90).multiply(math.pi / 180)
+
+    # slope steepness in range (eq. 2)
+    alpha_r = (alpha_sRad.tan().multiply(phi_rRad.cos())).atan()
+
+    # slope steepness in azimuth (eq 3)
+    alpha_az = (alpha_sRad.tan().multiply(phi_rRad.sin())).atan()
+
+    # local incidence angle (eq. 4)
+    theta_lia = (alpha_az.cos().multiply((theta_iRad.subtract(alpha_r)).cos())).acos()
+    theta_liaDeg = theta_lia.multiply(180 / math.pi)
+  
+    # 2.2 
+    # Gamma_nought_flat
+    gamma0 = sigma0Pow.divide(theta_iRad.cos())
+    gamma0dB = ee.Image.constant(10).multiply(gamma0.log10())
+    ratio_1 = gamma0dB.select('VV').subtract(gamma0dB.select('VH'))
+
+    # Volumetric Model
+    nominator = (ninetyRad.subtract(theta_iRad).add(alpha_r)).tan()
+    denominator = (ninetyRad.subtract(theta_iRad)).tan()
+    volModel = (nominator.divide(denominator)).abs()
+
+    # apply model
+    gamma0_Volume = gamma0.divide(volModel)
+    gamma0_VolumeDB = ee.Image.constant(10).multiply(gamma0_Volume.log10())
+
+    # we add a layover/shadow maskto the original implmentation
+    # layover, where slope > radar viewing angle 
+    alpha_rDeg = alpha_r.multiply(180 / math.pi)
+    layover = alpha_rDeg.lt(theta_i);
+
+    # shadow where LIA > 90
+    shadow = theta_liaDeg.lt(85)
+
+    # calculate the ratio for RGB vis
+    ratio = gamma0_VolumeDB.select('VV').subtract(gamma0_VolumeDB.select('VH'))
+
+    output = gamma0_VolumeDB.addBands(ratio).addBands(alpha_r).addBands(phi_s).addBands(theta_iRad)\
+			    .addBands(layover).addBands(shadow).addBands(gamma0dB).addBands(ratio_1)
+
+    return output.select(['VV', 'VH'], ['VV', 'VH']).set("system:time_start",date)
+
+
+def applySpeckleFilter(img):
+    vv = img.select('VV')
+    vh = img.select('VH')
+    vv = speckleFilter(vv).rename('VV');
+    vh = speckleFilter(vh).rename('VH');
+    return ee.Image.cat(vv,vh).copyProperties(img,['system:time_start']);
+
+
+def speckleFilter(image):
+    """ apply the speckle filter """
+    ksize = 3
+    enl = 7; 
+
+    # Convert image from dB to natural values
+    nat_img = toNatural(image);
+
+    # Square kernel, ksize should be odd (typically 3, 5 or 7)
+    weights = ee.List.repeat(ee.List.repeat(1,ksize),ksize);
+
+    # ~~(ksize/2) does integer division in JavaScript
+    kernel = ee.Kernel.fixed(ksize,ksize, weights, ~~(ksize/2), ~~(ksize/2), False);
+
+    # Get mean and variance
+    mean = nat_img.reduceNeighborhood(ee.Reducer.mean(), kernel);
+    variance = nat_img.reduceNeighborhood(ee.Reducer.variance(), kernel);
+
+    # "Pure speckle" threshold
+    ci = variance.sqrt().divide(mean);# square root of inverse of enl
+
+    # If ci <= cu, the kernel lies in a "pure speckle" area -> return simple mean
+    cu = 1.0/math.sqrt(enl);
+
+    # If cu < ci < cmax the kernel lies in the low textured speckle area
+    # -> return the filtered value
+    cmax = math.sqrt(2.0) * cu;
+
+    alpha = ee.Image(1.0 + cu*cu).divide(ci.multiply(ci).subtract(cu*cu));
+    b = alpha.subtract(enl + 1.0);
+    d = mean.multiply(mean).multiply(b).multiply(b).add(alpha.multiply(mean).multiply(nat_img).multiply(4.0*enl));
+    f = b.multiply(mean).add(d.sqrt()).divide(alpha.multiply(2.0));
+
+    # If ci > cmax do not filter at all (i.e. we don't do anything, other then masking)
+    # Compose a 3 band image with the mean filtered "pure speckle", 
+    # the "low textured" filtered and the unfiltered portions
+    out = ee.Image.cat(toDB(mean.updateMask(ci.lte(cu))),toDB(f.updateMask(ci.gt(cu)).updateMask(ci.lt(cmax))),image.updateMask(ci.gte(cmax)));	
+		
+    return out.reduce(ee.Reducer.sum());
+
+def addRatio(img):
+    vv = toNatural(img.select(['VV'])).rename(['VV']);
+    vh = toNatural(img.select(['VH'])).rename(['VH']);
+    ratio = vh.divide(vv).rename(['ratio']);
+    return ee.Image.cat(vv,vh,ratio).copyProperties(img,['system:time_start']);
+
+
+def toNatural(img):
+    """Function to convert from dB to natural"""
+    return ee.Image(10.0).pow(img.select(0).divide(10.0));
+		
+def toDB(img):
+    """ Function to convert from natural to dB """
+    return ee.Image(img).log10().multiply(10.0);
+
+
+def toBands(collection):
+    
+    def createStack(img,prev):
+	return ee.Image(prev).addBands(img)
+    
+    stack = ee.Image(collection.iterate(createStack,ee.Image(1)))
+    stack = stack.select(ee.List.sequence(1, stack.bandNames().size().subtract(1)));
+    return stack;
+
+
 
 def check_for_monitor_capacity():
 
